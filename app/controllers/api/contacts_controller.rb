8a025039c7ccf21a6ac1f0a1c07b858f57e2ffbf
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-07 22:01:43 +0100

    Set up and fulfil rubocop

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,50 +1,48 @@
-class Api::ContactsController < ApplicationController
-  def index
-    @contacts = Contact.all
-    render json: @contacts
-  end
+module Api
+  class ContactsController < ApplicationController
+    def index
+      @contacts = Contact.all
+      render json: @contacts
+    end
 
-  def show
-    @contact = Contact.find(params[:id])
-    render json: @contact, :include => [:emails]
-  end
+    def show
+      @contact = Contact.find(params[:id])
+      render json: @contact, include: [:emails]
+    end
 
-  def create
-    @contact = Contact.new(contact_params)
+    def create
+      @contact = Contact.new(contact_params)
 
-    if @contact.save
-      render json: @contact, status: 201
-    else
-      render error: {error: 'Unable to create contact'}
+      if @contact.save
+        render json: @contact, status: 201
+      else
+        render error: { error: 'Unable to create contact' }
+      end
+    end
+
+    def update
+      @contact = Contact.find(params[:id])
+
+      if @contact.update(contact_params)
+        render json: @contact
+      else
+        render error: { error: 'Unable to update contact' }
+      end
+    end
+
+    def destroy
+      @contact = Contact.find(params[:id])
+
+      if @contact.destroy
+      else
+        render error: { error: 'Unable to delete contact' }
+      end
+    end
+
+    private
+
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name)
     end
   end
-
-  def update
-    @contact = Contact.find(params[:id])
-
-    if @contact.update(contact_params)
-      render json: @contact
-    else
-      render error: {error: 'Unable to update contact'}
-    end
-  end
-
-  def destroy
-    @contact = Contact.find(params[:id])
-
-    if @contact.destroy
-    else
-      render error: {error: 'Unable to delete contact'}
-    end
-  end
-
-
-
-  private
-
-  def contact_params
-    params.require(:contact).permit(:first_name, :last_name)
-  end
-
-
 end
--- spec/requests/api/contacts_controller_spec.rb
+++ spec/requests/api/contacts_controller_spec.rb
@@ -2,26 +2,24 @@
 
 require 'rails_helper'
 
-RSpec.describe 'GET /api/contacts' do
+describe ContactsController do
   it 'returns a list of all contacts' do
     anna = create(:contact, first_name: 'Anna', last_name: 'A')
     create(:contact, first_name: 'Boris', last_name: 'B')
 
     get api_contacts_url
 
-    body = JSON.parse(@response.body)
+    body = JSON.parse(response.body)
     expect(body.count).to eq(2)
     expect(body[0]['id']).to eq(anna.id)
     expect(body[0]['first_name']).to eq(anna.first_name)
     expect(body[0]['last_name']).to eq(anna.last_name)
   end
-end
 
-RSpec.describe 'POST /api/contacts' do
   it 'creates a contact' do
     contact_params = attributes_for(:contact, first_name: 'Cameron', last_name: 'C')
 
-    post api_contacts_url, params: {contact: contact_params}
+    post api_contacts_url, params: { contact: contact_params }
 
     expect(response.status).to eq 201
     expect(Contact.last.first_name).to eq contact_params[:first_name]
