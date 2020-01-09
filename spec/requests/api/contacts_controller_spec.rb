fdf46113815bd3e93d5c1ac0be93200f5aa89a93
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-09 18:05:38 +0100

    Add some describe blocks to contacts conroller spec

--- spec/requests/api/contacts_controller_spec.rb
+++ spec/requests/api/contacts_controller_spec.rb
@@ -3,26 +3,30 @@
 require 'rails_helper'
 
 describe Api::ContactsController do
-  it 'returns a list of all contacts' do
-    anna = create(:contact, first_name: 'Anna', last_name: 'A')
-    create(:contact, first_name: 'Boris', last_name: 'B')
+  describe '#index' do
+    it 'returns a list of all contacts' do
+      anna = create(:contact, first_name: 'Anna', last_name: 'A')
+      create(:contact, first_name: 'Boris', last_name: 'B')
 
-    get api_contacts_url
+      get api_contacts_url
 
-    body = JSON.parse(response.body)
-    expect(body.count).to eq(2)
-    expect(body[0]['id']).to eq(anna.id)
-    expect(body[0]['first_name']).to eq(anna.first_name)
-    expect(body[0]['last_name']).to eq(anna.last_name)
+      body = JSON.parse(response.body)
+      expect(body.count).to eq(2)
+      expect(body[0]['id']).to eq(anna.id)
+      expect(body[0]['first_name']).to eq(anna.first_name)
+      expect(body[0]['last_name']).to eq(anna.last_name)
+    end
   end
 
-  it 'creates a contact' do
-    contact_params = attributes_for(:contact, first_name: 'Cameron', last_name: 'C')
+  describe '#create' do
+    it 'creates a contact' do
+      contact_params = attributes_for(:contact, first_name: 'Cameron', last_name: 'C')
 
-    post api_contacts_url, params: { contact: contact_params }
+      post api_contacts_url, params: { contact: contact_params }
 
-    expect(response.status).to eq 201
-    expect(Contact.last.first_name).to eq contact_params[:first_name]
+      expect(response.status).to eq 201
+      expect(Contact.last.first_name).to eq contact_params[:first_name]
+    end
   end
 end
 
