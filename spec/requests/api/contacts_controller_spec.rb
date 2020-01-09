b5f93f4f87cab328593a09740bb608acbdb8427d
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-09 21:44:00 +0100

    Update contacts_controller_spec.rb

--- spec/requests/api/contacts_controller_spec.rb
+++ spec/requests/api/contacts_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe Api::ContactsController do
   describe '#index' do
     it 'returns a list of all contacts' do
-      anna = create(:contact, first_name: 'Anna', last_name: 'A')
+      create(:contact, first_name: 'Anna', last_name: 'A')
       create(:contact, first_name: 'Boris', last_name: 'B')
 
       get api_contacts_url
@@ -14,13 +14,24 @@ describe Api::ContactsController do
 
       body = JSON.parse(response.body)
       expect(body.count).to eq(2)
+    end
+
+    it 'returns fields of an item' do
+      anna = create(:contact, first_name: 'Anna', last_name: 'A')
+
+      get api_contacts_url
+
+      body = JSON.parse(response.body)
 
       first_entry = body.first
       expect(first_entry['id']).to eq(anna.id)
       expect(first_entry['first_name']).to eq(anna.first_name)
       expect(first_entry['last_name']).to eq(anna.last_name)
+      expect(first_entry.key?('created_at')).to eq(false)
+      expect(first_entry.key?('update_at')).to eq(false)
     end
   end
+
 
   describe '#create' do
     it 'creates a contact' do
@@ -45,7 +56,8 @@ describe Api::ContactsController do
       expect(body['id']).to eq(anna.id)
       expect(body['first_name']).to eq(anna.first_name)
       expect(body['last_name']).to eq(anna.last_name)
+      expect(body.key?('created_at')).to eq(false)
+      expect(body.key?('update_at')).to eq(false)
     end
   end
 end
-
